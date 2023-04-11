915184c7c7f6c1d3f3f18d270c575ec397f9bda4
Sanjay kumaran <sanjaykumaran10@gmail.com>
2023-04-11 14:26:20 +0530

    Finish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,49 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { RouterModule, Routes } from '@angular/router';
+import { PredictionComponent } from './prediction/prediction.component';
+import { NgChartsModule } from 'ng2-charts';
+
+
+const routes: Routes= [
+  // {path:'',component:HomeComponent},
+  {path:'',component:RegisterComponent},
+  {path:'login',component:LoginComponent},
+  {path:'dashboard',component:DashboardComponent},
+  {path:'prediction',component:PredictionComponent}
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    NavbarComponent,
+    HomeComponent,
+    RegisterComponent,
+    PredictionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes),
+    FormsModule,
+    HttpClientModule,
+    NgChartsModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+
